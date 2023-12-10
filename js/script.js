30e5ebccca194b53c59c36222bdf0f1525e78811
alessandro-ricci-boolean <cqfp.official@gmail.com>
2023-12-10 18:09:10 +0100

    snack1 completed

--- js/script.js
+++ js/script.js
@@ -1,13 +1,26 @@
-const bici = [{
-    name: "bici1",
+const bike = [{
+    name: "bike1",
     weight: 10
 },
 {
-    name: "bici2",
+    name: "bike2",
     weight: 8
 },
 {
-    name: "bici3",
+    name: "bike3",
     weight: 11
 }
 ];
+
+let light_bike = bike[0].weight;
+
+bike.forEach((elem) => {
+    if(elem.weight < light_bike.weight){
+        light_bike = elem;
+    }
+});
+
+let {name, weight} = light_bike;
+
+let text = `La bici più leggera è la ${name} e pesa ${weight}`;
+console.log(text);
